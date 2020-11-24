07ce856b62298fef15327d26fc8435e715a99c11
garimagupta3 <garima.gupta@embold.io>
2020-11-24 13:51:56 +0530

    committed 2

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -18,7 +18,7 @@ public class Bot {
         builder.setToken(token);
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.playing("@Sokobot for info!"));
-        System.out.println("compilation error test! check!");
+        System.out.println("compilation error test! check! 2");
         builder.addEventListeners(new Commands());
         builder.build();
         System.out.println("webhook test again 2 1 1 2");
